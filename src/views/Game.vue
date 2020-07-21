2ac0422c2df4c44869d107628eb5156f850095ca
yancymin <senseslofter@gmail.com>
2020-07-21 17:56:28 +0800

    Update Game.vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -14,30 +14,10 @@
     <main>
       <img src="../assets/code-9685.png" class="code" />
       <div class="input-wrap">
-        <input
-          type="tel"
-          name="input-1"
-          id="input-1"
-          oninput="if(value.length>1)value=value.slice(0,1)"
-        />
-        <input
-          type="tel"
-          name="input-2"
-          id="input-2"
-          oninput="if(value.length>1)value=value.slice(0,1)"
-        />
-        <input
-          type="tel"
-          name="input-3"
-          id="input-3"
-          oninput="if(value.length>1)value=value.slice(0,1)"
-        />
-        <input
-          type="tel"
-          name="input-4"
-          id="input-4"
-          oninput="if(value.length>1)value=value.slice(0,1)"
-        />
+        <input type="tel" name="input-1" id="input-1" maxlength="5" />
+        <input type="tel" name="input-2" id="input-2" maxlength="5" />
+        <input type="tel" name="input-3" id="input-3" maxlength="5" />
+        <input type="tel" name="input-4" id="input-4" maxlength="5" />
       </div>
     </main>
   </div>
